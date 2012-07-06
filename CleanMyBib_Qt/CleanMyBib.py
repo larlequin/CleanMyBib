f114b13747dfee1031a9fbf154b4aec49efe5ff1
GT Vallet <gtvallet@gmail.com>
2012-07-06 14:18:43 -0400

    New version of the script and of the GUI

---
+++ CleanMyBib_Qt/CleanMyBib.py
@@ -0,0 +1,319 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# A Python class to format bibtex references.
+# From a provided bibtex file, the class will adapt the format for the journal
+#   name and the page numbers into short or long format.
+# Give a new cleaned file with the minimal required fields displayed nicely.
+#
+# G.T. Vallet -- University Lyon 2
+# 2012/07/04  -- v04
+# GPLv3
+
+import re
+import os
+import sys
+import csv
+import platform
+import textwrap
+from pybtex.database.input import bibtex
+
+
+class CleanFileBib():
+    """ A class to clean a bibtex file to format the journal name and the pages
+          numbers into long or short format.
+        Create a new file with minimal required fields.
+    """
+
+    def __init__(self, fileBib, file_BibOk, fields, journal_style = "long",
+                     page_style="long"):
+        """ Extract the references from the bibtex file,
+            Format the page numbers and the journal name,
+            Log missing data,
+            Write the correct fields according to the reference type.
+        """
+        # Initiate a list to log missing page numbers
+        log_pageNber = []
+        noYear = ["in press", "accepted", "sous presse", "accepte"]
+        noPage = ["book", "manual"]
+        # Parse the bibtex file
+        parser, citeKeys = self.parseBib(fileBib)
+        # For each reference, change the format if needed and select the
+        #   remaining fields to use
+        for citeKey in citeKeys:
+            ref =  parser.data.entries[citeKey]  # Select the current reference
+            if ref.fields['year'].lower() not in noYear:
+                try:  # Check/Change the page numbers format
+                    pages = self.ChangePageNber(ref.fields['pages'], page_style)
+                except KeyError:
+                    if ref.type not in noPage:
+                        log_pageNber.append(citeKey)
+                if len(pages) == 0 and ref.type not in noPage:
+                    log_pageNber.append(citeKey)
+            journal = ""
+            # Initiate some lists for the logs
+            log_journals = []
+            # Format the articles
+            if ref.type == 'article':
+                journal, fieldsToFill, log_journals = self.cleanArticle(citeKey,
+                                       ref, journal_style, fields, log_journals)
+            else:
+                # Fields to exclude according to the reference kind
+                other_exclude = ['pages', 'volume']
+                fieldsToFill = []
+                for field in fields:
+                    if field not in other_exclude:
+                        fieldsToFill.append(field)
+            data = self.fillRef(ref, fieldsToFill, journal, pages)
+            lenMax = self.fieldLengthMax(fields)
+            file_BibOk.write(self.refPrint(citeKey, ref, data, lenMax))
+            if len(log_journals)>0 or len(log_pageNber)>0:
+                self.logFile(fileBib, log_pageNber, log_journals)
+
+
+# -------------------------------------------------------------
+#  PARSE THE BIBTEX FILE
+# -------------------------------------------------------------
+    def parseBib(self, fileBib):
+        """ Use the bibtex parser to retrieve the references of the input file
+            Return the parser and the citeKeys sorted.
+        """
+        parser = bibtex.Parser()
+        bibData = parser.parse_file(fileBib)
+        return parser, sorted(bibData.entries.keys())
+
+
+# -------------------------------------------------------------
+#  PREPARE ARTICLE FORMATING
+# -------------------------------------------------------------
+    def cleanArticle(self, citeKey, ref, journal_style, fields, log_journals):
+        """ A function to prepare which fields will be used for articles and
+              format the journal name.
+            Return the journal name and the list of fields to use
+        """
+        # Load journal names database
+        journals = self.Read_CSV('.img/Journals.csv', 0, 1)
+        # Initiate the list of fields to use for the reference
+        fieldsToFill = []
+        # Define which fields should be exclude for an article
+        article_exclude = ['booktitle', 'editor', 'publisher', 'address']
+        # Change the format of the journal name
+        try:
+            journal = self.Journal_Format(ref.fields['journal'].lower(),
+                                             journals, journal_style)
+            journal = self.Journal_Name(journal)
+        except KeyError:
+            log_journals.append(citeKey)
+        # Define the fields to keep and fill the data into a list
+        for field in fields:
+            if field not in article_exclude:
+                fieldsToFill.append(field)
+        return journal, fieldsToFill, log_journals
+
+
+# -------------------------------------------------------------
+#  JOURNAL NAME FORMATING
+# -------------------------------------------------------------
+    def Read_CSV(self, file_name, col1=1, col2=2):
+        """ A function to read a csv file into a dictionary
+            Take the file name 'ex.csv' and the columns to read
+            Give the dictionary as output
+        """
+        CSV_file = open(file_name, "rb")
+        dico = {}
+        try:
+            reader = csv.reader(CSV_file, delimiter=',')
+            for row in reader:
+                dico[row[col1].lower()] = row[col2].lower()
+        finally:
+            CSV_file.close()
+        return dico
+
+
+    def Journal_Format(self, journal_name, journals, style='long'):
+        """ Change name of the Journal according to APA/Vancouver style
+        """
+        # Style 'long' like in APA or 'short' like in Vancouver
+        journal_name = journal_name.replace(".","")
+        if style == 'long':
+            # Reverse the dictionary (replace long name by short one)
+            if journal_name in journals.values():
+                dico_inv   = dict([(v, k) for (k, v) in journals.iteritems()])
+                journal_ok = dico_inv[journal_name]
+            else:
+                journal_ok = journal_name
+        elif style == 'short':
+            if journal_name in journals.keys():
+                journal_ok = journals[journal_name]
+            else:
+                journal_ok = journal_name
+            # journal_ok = " ".join(journal_ok)
+        else:
+            journal_ok = journal_name
+        return journal_ok
+
+
+    def Journal_Name(self, journal_name):
+        """ A function to add a capital letter of each words of the journal name
+            Expect for the liaison words
+        """
+        # List of the words or strings to not capitalize
+        to_pass = ['et', 'le', 'la', 'de', 'du', 'l', 'a', 'an', 'in', 'to', \
+                    'of', 'for', 'the', 'and', '-', '--', ':', '?']
+        words = journal_name.split() # Split the journal's name (single word)
+        words_ok = []                # Initiate an empty list
+        words_ok.append( words[0].capitalize() ) # Capitalized the first word
+        for curWord in words[1:]:
+            if curWord in to_pass:          # Capitalize the good words
+                words_ok.append( curWord )  # Avoid the liaison words
+            else:
+                words_ok.append( curWord.capitalize() )
+        journal_name_ok = ""     # Prepare the final name
+        for i in words_ok:
+            if i == words_ok[len(words_ok)-1]: # Construct the final name
+                journal_name_ok += i        # Avoid adding a 'space' at the end
+            else:
+                journal_name_ok += i+" "
+        return journal_name_ok
+
+
+# -------------------------------------------------------------
+#  PAGE NUMBERS FORMATING
+# -------------------------------------------------------------
+    def PagesNber(self, t1, t2, style='long'):
+        """ A function to correct the pages numbers
+            In the long style format both page numbers have the same length
+            In the short style format, the repeated first numbers are deleted
+            First page is t1, last page is t2
+            E.g.   Long (APA): 1128--35   transformed to 1128--1135
+            E.g. Short (Vanc): 1128--1135 transformed to 1128--35
+        """
+        if style == 'long':
+            while len(t1) > len(t2):     # Check if a correction is needed
+                t2 = t1[-(1+len(t2))]+t2 # Add the missing numbers
+        else:
+            x, done = 0, 0         # Initiate control variables
+            if len(t1) >= len(t2): # Check the pages format
+                while done < 1:
+                    if t1[x] == t2[x]: # Find how many number are identical
+                        x += 1         # Increment control variable
+                    else:
+                        done = 1   # Exit the loop when numbers are different
+                t2 = t2[x:]        # Correct the last page number value
+        return t2
+
+
+    def ChangePageNber(self, txt, style='long'):
+        """ A function to apply the correct change page number,
+              either regarding the APA or Vancouver guidelines.
+            Find the groups of pages and send them to the APA/Vancouver function
+        """
+        # Extract the page numbers
+        pagePattern= re.compile(r'\D*(\d+)\D*(\d*)\D*$')
+        if pagePattern.search(txt) != None:
+            pages= pagePattern.search(txt).groups() # Find the groups of numbers
+            p1 = pages[0]  # Define p1 and p2
+            p2 = pages[1]  # p1= first page; p2= last page
+            if len(p2) > 0: # Check if a number for t2 exists
+                t2 = self.PagesNber(p1, p2, style) # t2 is the new value of p2
+            else:         # If no number for t2, then print XXXX
+                t2 = 'XXXX'
+            total  = p1 + '--' + t2  # Construct the final pages line
+        else:
+            total = ""
+        return total
+
+
+# -------------------------------------------------------------
+#  PREPARE THE FINAL FILE
+# -------------------------------------------------------------
+    def fillRef(self, ref, fieldsOK, journal_ok, pages_ok):
+        data = {}
+        for field in fieldsOK:
+            if field == "journal" and len(journal_ok)>0:
+                data[field] = journal_ok
+            elif field == "pages" and len(pages_ok)>0:
+                data[field] = pages_ok
+            else:
+                try:
+                    data[field] = ref.fields[field].encode('utf-8')
+                except KeyError:
+                    pass
+        return data
+
+
+    def fieldLengthMax(self, fields):
+        lenMax = 0
+        for field in fields:
+            if len(field)>lenMax:
+                lenMax = len(field)
+        return lenMax
+
+
+    def refPrint(self, citeKey, ref, data, lenMax):
+        """ A function to format the printed reference into the bibtex rules
+            Give a nice readable organization of the file
+            Capitalized the first letter of the field names
+        """
+        header = "@{0}{{{1},".format(ref.type, citeKey)
+        body   = ""
+        for field in sorted(data.keys()):  # Loop to fill the reference
+            body = body + "\n\t"
+            spaces = ""
+            if len(field)<lenMax:  # Harmonization of the fields spaces
+                index = 0
+                while index < lenMax-len(field):
+                    spaces = spaces + " "
+                    index += 1
+            body = body + field.capitalize() + spaces + "=  {" +data[field]+"},"
+        body = body[0:-1]  # Deleted the last comma
+        footer = "\n}}\n\n"
+        return header+body+footer
+
+
+# -------------------------------------------------------------
+#  LOG FILE
+# -------------------------------------------------------------
+    def logFile(self, fileBib, pageNber, journals):
+        # Create a log file for more information about the bibfile
+        rep, name  = os.path.split(fileBib)
+        log = open(rep+"CleanMyBib.log", "w")
+        if  len(journals)>0:
+            log.write("\nMissing Journal Name\n--------------------\n")
+            for ref in journals:
+                log.write("Missing journal name for the reference '{0}'\n"
+                                                                .format(ref))
+        if len(pageNber)>0:
+            log.write("\nMissing Page Numbers\n--------------------\n")
+            for ref in pageNber:
+                log.write("Missing page numbers for the reference '{0}'\n"
+                                                                .format(ref))
+        log.close()
+
+
+# --------------------------------------------------------------
+# START THE APPLICATION
+# --------------------------------------------------------------
+def main():
+    """ If the script is called as a main script, define general options and
+          launch the class to clean the file.
+    """
+    # Select the bibtex file
+    try:
+        fileBib = sys.argv[1]  # Input file
+    except IndexError:
+        print "No bibtex file provided"
+        print "Assuming the file to be 'MyCollection.bib'"
+        fileBib = "MyCollection.bib"
+    fileOK  = open('bibcleaned.bib', 'w')
+    fields = ['abstract','author','year','title', 'journal', 'booktitle', \
+                'pages', 'volume', 'editor','publisher','address']
+    journal_style = "long"
+    page_style    = "long"
+    app = CleanFileBib(fileBib, fileOK, fields, journal_style, page_style)
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ CleanMyBib_Qt/CleanMyBib_Qt.py
@@ -0,0 +1,322 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import platform
+import re
+import csv
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+from CleanMyBib import CleanFileBib
+from pybtex.database.input import bibtex
+
+__version__ = "4.0.0"
+
+
+# Allow to read the picture files in pyinstaller
+datadir = ".img"
+if not hasattr(sys, "frozen"):   # not packed
+    datadir = os.path.join(os.path.dirname(__file__), datadir)
+elif "_MEIPASS2" in os.environ:   # one-file temp's directory
+    datadir = os.path.join(os.environ["_MEIPASS2"], datadir)
+else:   # one-dir
+    datadir = os.path.join(os.path.dirname(sys.argv[0]), datadir)
+
+
+# --------------------------------------------------------------
+# GRAPHICAL INTERFACE FOR CLEAN MY BIB
+# --------------------------------------------------------------
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        """ Define the main widgets and options
+        """
+        super(MainWindow, self).__init__()
+        # Default fields to keep in the cleaned file
+        self.chx = ['abstract','author','year','title','booktitle', 'journal',\
+                            'pages', 'volume', 'editor','publisher','address']
+        # Create the main frame to handle the widgets
+        self.mainWidget=QtGui.QWidget(self)
+        self.setCentralWidget(self.mainWidget)
+        self.grid = QtGui.QGridLayout(self.mainWidget) # Define a grid
+        self.setLayout(self.grid)
+        # Create a status bar
+        self.status = self.statusBar()
+        self.status.showMessage("Ready", 5000)
+        # Call the menu, options and status bar
+        self.menu()
+        self.style_block()
+        self.bibFile()
+        self.statusBib()
+        # Define the main window size and name
+        self.setWindowTitle('Clean My Bib')
+        self.show()
+
+
+    def menu(self):
+        """ Define the action in the Menu and Toolbar
+        """
+        # Options
+        options = QtGui.QAction(QtGui.QIcon('opt.jpeg'), 'Options', self)
+        options.setShortcut('Ctrl+O')
+        options.setStatusTip('Change the fields to ignore')
+        options.triggered.connect(self.Opts)
+        # Exit
+        exitAction = QtGui.QAction(QtGui.QIcon('exit2.jpeg'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+        # About
+        aboutAction = QtGui.QAction(QtGui.QIcon('about.jpeg'), 'About', self)
+        aboutAction.setStatusTip('About Clean My Bib')
+        aboutAction.triggered.connect(self.about)
+        # Fill the Menu
+        menubar  = self.menuBar()
+        mainMenu = menubar.addMenu('&Menu')
+        mainMenu.addAction(options)
+        mainMenu.addAction(aboutAction)
+        mainMenu.addAction(exitAction)
+
+
+    def style_block(self):
+        """ Define the section of the GUI dedicated to the style format
+            User can choose between the 'Long' and 'Short' style for the journal
+              name and the page numbers.
+            An option is provided to add or not the DOIs
+        """
+        # Create a ComboBox to select the journal name's style
+        cx_journal_style = QtGui.QComboBox(self)
+        list_style = [" Long format", "Short format"]
+        cx_journal_style.addItems(list_style)
+        cx_journal_style.SizeAdjustPolicy(1)
+        # Create a ComboBox to select the page numbers' style
+        cx_pages_style = QtGui.QComboBox(self)
+        cx_pages_style.addItems(list_style)
+        cx_pages_style.SizeAdjustPolicy(1)
+        # Create a checkbox for the DOIs
+        self.add_doi = QtGui.QCheckBox("Add DOIs")
+        # Define some Logo and Labels to display information to the user
+        logo1 = QtGui.QLabel(self)
+        icon1 = QtGui.QPixmap(datadir+"/nb1.png")
+        logo1.setPixmap(icon1)
+        first_step  = QtGui.QLabel("<b><font color ='darkblue'><h2>&nbsp; \
+                                  &nbsp; &nbsp; &nbsp; Define a style</b></h2>")
+        lab_style   = QtGui.QLabel("<b>Journal</b>", self)
+        lab_format  = QtGui.QLabel("<b>Pages</b>", self)
+        lab_example = QtGui.QLabel("<b>DOI</b>", self)
+        self.lab_empty = QtGui.QLabel("", self)
+        self.lab_empty.setMinimumSize(90, 25)
+        # Place the widgets on the grid.
+        self.grid.addWidget(logo1, 0, 1, 1, 2)
+        self.grid.addWidget(first_step, 0, 1, 1, 2)
+        self.grid.addWidget(lab_style, 2, 1)
+        self.grid.addWidget(cx_journal_style, 2, 2)
+        self.grid.addWidget(lab_format, 3, 1)
+        self.grid.addWidget(lab_example, 4, 1)
+        self.grid.addWidget(cx_pages_style, 3, 2)
+        self.grid.addWidget(self.add_doi, 4, 2)
+        # Control the style and the choice done
+        self.journal_style = "long"
+        self.pages_style = "long"
+        cx_journal_style.activated[int].connect(self.styleJournal)
+        cx_pages_style.activated[int].connect(self.stylePages)
+        self.mainWidget.connect(self.add_doi,
+                                QtCore.SIGNAL('stateChanged(int)'), self.doi)
+
+
+    def doi(self):
+        """ Add and remove the doi fields in the list of fields to keep
+        """
+        if self.add_doi.isChecked():
+            self.chx.append("doi")
+        else:
+            if "doi" in self.chx:
+                self.chx.remove("doi")
+
+
+    def styleJournal(self, style):
+        if style == 0:
+            self.journal_style = "long"
+        else:
+            self.journal_style = "short"
+
+
+    def stylePages(self, style):
+        if style == 0:
+            self.pages_style = "long"
+        else:
+            self.pages_style = "short"
+
+
+    def bibFile(self):
+        """ GUI section to receive a dropped file
+            And take it as the bib file to clean
+        """
+        self.setAcceptDrops(True)
+        # Define a picture where to drop the file
+        self.dropIcon = QtGui.QLabel(self)
+        dragdrop = QtGui.QPixmap(datadir+"/drop.png")
+        self.dropIcon.setPixmap(dragdrop)
+        self.dropIcon.setAlignment(QtCore.Qt.AlignCenter)
+        # Define some Logo and Labels to display information to the user
+        logo2 = QtGui.QLabel(self)
+        icon2 = QtGui.QPixmap(datadir+"/nb2.png")
+        logo2.setPixmap(icon2)
+        second_step = QtGui.QLabel("<b><font color ='darkblue'><h2>&nbsp; \
+                                        &nbsp; &nbsp; &nbsp; Bibtex file</b></h2>")
+        lab_drop  = QtGui.QLabel("<b><h3>Drop a bib file here</b></h3>", self)
+        lab_drop.setAlignment(QtCore.Qt.AlignCenter)
+        # Place the widgets on the grid
+        self.grid.addWidget(self.lab_empty, 2, 3, 1, 5)  # Add an empty column
+        self.grid.addWidget(self.lab_empty, 2, 4, 1, 5)  # Add an empty column
+        self.grid.addWidget(logo2, 0, 6, 1, 2)
+        self.grid.addWidget(second_step, 0, 6, 1, 2)
+        self.grid.addWidget(self.dropIcon, 2, 6, 2, 3)
+        self.grid.addWidget(lab_drop, 4, 6, 1, 3)
+
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+
+    def dropEvent(self, event):
+        """ Extract the path of the dropped file
+            Call the CleanMyBib script and update the status bar
+        """
+        for url in event.mimeData().urls():
+            path = url.toLocalFile().toLocal8Bit().data()
+            if os.path.isfile(path):
+                # Extract the path and open the cleaned file
+                rep, name  = os.path.split(path)
+                name_bibOk = 'Cleaned_'+name
+                fileBibOK  = open(os.path.join(rep, name_bibOk), 'w')
+                # Update the status bar
+                self.status.showMessage("File to clean: "+name, 5000)
+                # Prepare the fields to keep in the final file
+                fields = []
+                for item in self.chx:
+                    fields.append(item.lower())
+                try:
+                    CleanFileBib(path, fileBibOK, fields, self.journal_style, self.pages_style)
+                    self.statusClean.setText("File cleaned successfully!")
+                    icon4 = QtGui.QPixmap(datadir+"/success.png")
+                    self.waitLogo.setPixmap(icon4)
+                    self.status.showMessage("Drop another file", 5000)
+                except:
+                    self.statusClean.setText("An error has occurred.\
+                        \nPlease check your bibtex file")
+                    icon5 = QtGui.QPixmap(datadir+"/error.png")
+                    self.waitLogo.setPixmap(icon5)
+                fileBibOK.close()
+
+
+    def statusBib(self):
+        """ The third panel of the main frame is used to display the current
+              status of the file to be cleaned
+        """
+        logo3 = QtGui.QLabel(self)
+        icon3 = QtGui.QPixmap(datadir+"/nb3.png")
+        logo3.setPixmap(icon3)
+        third_step = QtGui.QLabel("<b><font color ='darkblue'><h2>&nbsp; &nbsp; \
+                                     &nbsp; &nbsp; Clean my bib...</b></h2>")
+        self.statusClean = QtGui.QLabel("<i>&nbsp;<BR>Ready to receive <BR>a bibtex file</i>")
+        self.statusClean.setAlignment(QtCore.Qt.AlignCenter)
+        self.waitLogo    = QtGui.QLabel(self)
+        self.icon4 = QtGui.QPixmap(datadir+"/wait.png")
+        self.waitLogo.setPixmap(self.icon4)
+        self.waitLogo.setAlignment(QtCore.Qt.AlignCenter)
+        # Display the widgets on the grid
+        self.grid.addWidget(logo3, 0, 12, 1, 2)
+        self.grid.addWidget(third_step, 0, 12, 1, 2)
+        self.grid.addWidget(self.statusClean, 1, 12, 2, 3)
+        self.grid.addWidget(self.waitLogo, 3, 12, 1, 2)
+
+
+    def about(self):
+        QtGui.QMessageBox.about(self, "About Clean My Bib",
+        """<b>Clean My Bib</b> v %s
+        <p><b>Licence:</b> GPLv3 by GT Vallet
+        <p>This application can be used to prepare a bibtex
+        file to prepare the journal name and page numbers format into short or
+        long forms.
+        <p>Python %s - on %s""" % (
+            __version__, platform.python_version(), platform.system()))
+
+
+    def Opts(self):
+        """ Option panel to add/remove key words defining the fields
+             to add in the cleaned bibtex file
+        """
+        opt = QtGui.QDialog(self)
+        opt.setWindowTitle('Options -- Fields to keep')
+        self.listOpt = QtGui.QListWidget(opt)
+        for item in sorted(self.chx):
+                self.listOpt.addItem(item.capitalize())
+        # Define the buttons
+        AddBt = QtGui.QPushButton('Add', opt)
+        RemBt = QtGui.QPushButton('Remove', opt)
+        QtBt  = QtGui.QPushButton('Quit', opt)
+        Cl_Bt = QtGui.QPushButton('Cancel', opt)
+        # Define the action associated to the buttons
+        RemBt.clicked.connect(self.RemoveField)
+        AddBt.clicked.connect(self.Add)
+        Cl_Bt.clicked.connect(opt.close)
+        QtBt.clicked.connect(opt.close)
+        QtBt.clicked.connect(self.UpList)
+        # Place the widgets on the grid
+        grid_opt = QtGui.QGridLayout()
+        grid_opt.addWidget(self.listOpt, 0, 0, 5, 3)
+        grid_opt.addWidget(AddBt, 0, 3)
+        grid_opt.addWidget(RemBt, 1, 3)
+        grid_opt.addWidget(QtBt, 5, 3)
+        grid_opt.addWidget(Cl_Bt, 5, 2)
+        # Show the option window
+        opt.setLayout(grid_opt)
+        opt.show()
+
+
+    def Add(self):
+        """ Add a new field to the list
+        """
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog',
+            'Add a field:')
+
+        if ok:
+            self.listOpt.addItem(str(text))
+            self.listOpt.sortItems(order = QtCore.Qt.AscendingOrder)
+
+
+    def RemoveField(self):
+        """ Remove a field for the list
+        """
+        index = self.listOpt.currentRow()
+        self.listOpt.takeItem(index)
+        self.listOpt.sortItems(order = QtCore.Qt.AscendingOrder)
+
+
+    def UpList(self):
+        """ Finally update the list of field to send back to the program
+        """
+        self.chx = []
+        for index in xrange(self.listOpt.count()):
+            self.chx.append(str(self.listOpt.item(index).text()))
+
+
+
+# --------------------------------------------------------------
+# START THE APPLICATION
+# --------------------------------------------------------------
+def main():
+    """Define the main application
+       Calling the UI
+    """
+    app = QtGui.QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
